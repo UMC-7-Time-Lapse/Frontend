da4bc250c60c9e7900ae979cff29986f9cdfc56c
riwon <137711369+cherry-p0p@users.noreply.github.com>
2024-07-04 17:37:11 +0900

    [Docs] 로그인 페이지 디자인 수정

--- Frontend/Frontend/LoginView.swift
+++ Frontend/Frontend/LoginView.swift
@@ -3,86 +3,88 @@ import KakaoSDKAuth
 import KakaoSDKUser
 
 // 로그인 화면
-
 struct LoginView: View {
     @State private var userName: String = ""
     @State private var userMail: String = ""
     @State private var profileImage: URL?
     @State private var loginStatus: String = ""
+    @State private var isLoggedIn: Bool = false
 
     var body: some View {
-        ZStack {
+        VStack(spacing: 50) {
+            VStack(alignment: .leading) {
+                HStack {
+                    Text("5초")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color(hex: "#2E83F2"))
+                    Text("만에")
+                        .font(.title)
+                }
+                Text("타임캡슐을 만들어 봐요")
+                    .font(.title)
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding(.leading)
+
+            Button(action: kakaoLogin) {
+                HStack {
+                    Image("kakao_login_large_wide")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(height: 50)
+                }
+                .frame(width: UIScreen.main.bounds.width * 0.8)
+                .background(Color(hex: "#FEE500"))
+                .cornerRadius(10)
+            }
             
-            VStack(spacing: 30) {
-                VStack {
-                    Text("5초만에")
-                    Text("타임캡슐을 만들어 봐요")
+            if !loginStatus.isEmpty {
+                Text(loginStatus)
+                    .font(.headline)
+                    .foregroundColor(.green)
+            }
+
+            
+            // 공지사랑, 고객센터
+            VStack {
+                HStack(alignment: .center) {
+                    Text("공지 사항")
+                    Text("l")
+                    Text("고객 센터")
                 }
-                .font(.largeTitle)
-                .fontWeight(.bold)
-                .foregroundColor(Color(hex: "#392020"))
-                .padding(.bottom, 30)
-                
-                Button(action: kakaoLogin) {
-                    HStack {
-                        Image("kakao_login_large_wide")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(height: 50)
-                    }
-                    .frame(width: UIScreen.main.bounds.width * 0.9)
-                    .background(Color(hex: "#FEE500"))
-                    .cornerRadius(8)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 10)
-                            .stroke(Color(hex: "#392020"), lineWidth: 2)
-                    )
-                }
-                
-                if !loginStatus.isEmpty {
-                    Text(loginStatus)
-                        .font(.headline)
-                        .foregroundColor(.green)
-                }
-                
-                Button(action: kakaoLogout) {
-                    Text("로그아웃")
-                        .font(.headline)
-                        .foregroundColor(.white)
-                        .padding()
-                        .frame(width: UIScreen.main.bounds.width * 0.9)
-                        .background(Color.red)
-                        .cornerRadius(10)
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 10)
-                                .stroke(Color(hex: "#392020"), lineWidth: 2)
-                        )
-                }
-                
-                if !userName.isEmpty {
-                    Text("Name: \(userName)")
-                        .font(.headline)
-                        .foregroundColor(.black)
-                }
-                
-                if !userMail.isEmpty {
-                    Text("Email: \(userMail)")
-                        .font(.headline)
-                        .foregroundColor(.gray)
-                }
-                
-                if let profileImage = profileImage {
-                    AsyncImage(url: profileImage) { image in
-                        image.resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 100, height: 100)
-                            .clipShape(Circle())
-                    } placeholder: {
-                        ProgressView()
-                    }
+                .font(.title3)
+                .foregroundColor(.gray)
+            }
+            
+    
+            
+            if !userName.isEmpty {
+                Text("Name: \(userName)")
+                    .font(.headline)
+                    .foregroundColor(.black)
+            }
+            
+            if !userMail.isEmpty {
+                Text("Email: \(userMail)")
+                    .font(.headline)
+                    .foregroundColor(.gray)
+            }
+            
+            if let profileImage = profileImage {
+                AsyncImage(url: profileImage) { image in
+                    image.resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 100, height: 100)
+                        .clipShape(Circle())
+                } placeholder: {
+                    ProgressView()
                 }
             }
-            .padding()
+        }
+        .padding()
+        .fullScreenCover(isPresented: $isLoggedIn) {
+            MainView()
         }
     }
     
@@ -95,6 +97,7 @@ struct LoginView: View {
                     print(oauthToken)
                     loginStatus = "로그인 성공"
                     fetchUserInfo()
+                    isLoggedIn = true // 로그인 성공 시 MainView로 이동
                 }
             }
         } else {
@@ -105,6 +108,7 @@ struct LoginView: View {
                     print(oauthToken)
                     loginStatus = "로그인 성공"
                     fetchUserInfo()
+                    isLoggedIn = true // 로그인 성공 시 MainView로 이동
                 }
             }
         }
@@ -123,21 +127,15 @@ struct LoginView: View {
             }
         }
     }
-    
-    func kakaoLogout() {
-        UserApi.shared.logout { (error) in
-            if let error = error {
-                print(error)
-            } else {
-                print("로그아웃 성공")
-                loginStatus = "로그아웃 성공"
-                userName = ""
-                userMail = ""
-                profileImage = nil
-            }
-        }
+}
+
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
     }
 }
+
 
 #Preview {
     LoginView()
